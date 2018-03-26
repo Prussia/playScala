ff9a5a663b6955b1a17209e4e14a06defb5a72f0
Prussia <prussia.hu@pwc.com>
2018-03-26 12:48:02 +0800

    update

--- playScala/src/main/scala/com/prussia/play/scala/Main.scala
+++ playScala/src/main/scala/com/prussia/play/scala/Main.scala
@@ -2,5 +2,8 @@ package com.prussia.play.scala
 
 object Main extends App {
   val ages = Seq(42, 75, 29, 64)
+  println(s"********************************")
   println(s"The oldest person is ${ages.max}")
+  println(s"The youngest person is ${ages.min}")
+  println(s"********************************")
 }
